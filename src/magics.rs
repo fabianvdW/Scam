de2d66b156feb51feef127cd988b35f487da3ddf
Terje <42723273+TerjeKir@users.noreply.github.com>
2020-12-08 08:58:29 +0100

    oops

--- src/magics.rs
+++ src/magics.rs
@@ -5,9 +5,9 @@ include!(concat!(env!("OUT_DIR"), "/magic_attacks.rs"));
 impl Magic {
     pub fn bishop_attacks(sq: Square, occ: BitBoard) -> BitBoard {
         if cfg!(all(target_arch = "x64_64", target_feature = "bmi2")) {
-            BitBoard(ATTACKS[BISHOP_MAGICS[sq as usize].apply_magic(occ)])
-        } else {
             BitBoard(ATTACKS[BISHOP_MAGICS[sq as usize].apply_bmi2(occ)])
+        } else {
+            BitBoard(ATTACKS[BISHOP_MAGICS[sq as usize].apply_magic(occ)])
         }
     }
 
