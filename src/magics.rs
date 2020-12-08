8d032d986737f558f688ab4d3047775fd62f58c9
Terje <42723273+TerjeKir@users.noreply.github.com>
2020-12-08 08:53:09 +0100

    cfg!

--- src/magics.rs
+++ src/magics.rs
@@ -3,23 +3,19 @@ use crate::types::*;
 
 include!(concat!(env!("OUT_DIR"), "/magic_attacks.rs"));
 impl Magic {
-    #[cfg(all(target_arch = "x86_64", target_feature = "bmi2"))]
     pub fn bishop_attacks(sq: Square, occ: BitBoard) -> BitBoard {
-        BitBoard(ATTACKS[BISHOP_MAGICS[sq as usize].apply_bmi2(occ)])
+        if cfg!(all(target_arch = "x64_64", target_feature = "bmi2")) {
+            BitBoard(ATTACKS[BISHOP_MAGICS[sq as usize].apply_magic(occ)])
+        } else {
+            BitBoard(ATTACKS[BISHOP_MAGICS[sq as usize].apply_bmi2(occ)])
+        }
     }
 
-    #[cfg(not(all(target_arch = "x86_64", target_feature = "bmi2")))]
-    pub fn bishop_attacks(sq: Square, occ: BitBoard) -> BitBoard {
-        BitBoard(ATTACKS[BISHOP_MAGICS[sq as usize].apply_magic(occ)])
-    }
-
-    #[cfg(all(target_arch = "x86_64", target_feature = "bmi2"))]
     pub fn rook_attacks(sq: Square, occ: BitBoard) -> BitBoard {
-        BitBoard(ATTACKS[ROOK_MAGICS[sq as usize].apply_bmi2(occ)])
-    }
-
-    #[cfg(not(all(target_arch = "x86_64", target_feature = "bmi2")))]
-    pub fn rook_attacks(sq: Square, occ: BitBoard) -> BitBoard {
-        BitBoard(ATTACKS[ROOK_MAGICS[sq as usize].apply_magic(occ)])
+        if cfg!(all(target_arch = "x64_64", target_feature = "bmi2")) {
+            BitBoard(ATTACKS[ROOK_MAGICS[sq as usize].apply_bmi2(occ)])
+        } else {
+            BitBoard(ATTACKS[ROOK_MAGICS[sq as usize].apply_magic(occ)])
+        }
     }
 }
