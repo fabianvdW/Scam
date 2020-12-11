9d4cb71dcd71546e21557cc9a099caef880927a9
TerjeKir <42723273+TerjeKir@users.noreply.github.com>
2020-12-11 22:43:33 +0100

    Rest of the attack functions(#8)
    Adds attack functions for the remaining piece types knight, king, pawn and queen and some utility functions to use them.
    Also refactors the Magic code out of types.rs into its own file.

---
+++ src/attacks.rs
@@ -0,0 +1,63 @@
+use crate::bitboard::*;
+use crate::magics::*;
+use crate::types::*;
+
+include!(concat!(env!("OUT_DIR"), "/magic_attacks.rs"));
+
+const KING_ATTACKS: [BitBoard; 64] = init_non_slider_attacks(KING_DIRS);
+const KNIGHT_ATTACKS: [BitBoard; 64] = init_non_slider_attacks(KNIGHT_DIRS);
+
+pub fn pawn_attack_bb(c: Color, sq: Square) -> BitBoard {
+    pawn_bb_attack_bb(c, bb!(sq))
+}
+
+pub fn pawn_bb_attack_bb(c: Color, pawns: BitBoard) -> BitBoard {
+    let up = if c == WHITE { NORTH } else { SOUTH };
+    pawns.shift(up + WEST) | pawns.shift(up + EAST)
+}
+
+pub fn attack_bb(pt: PieceType, sq: Square, occ: BitBoard) -> BitBoard {
+    match pt {
+        KNIGHT => knight_attacks(sq),
+        BISHOP => bishop_attacks(sq, occ),
+        ROOK => rook_attacks(sq, occ),
+        QUEEN => bishop_attacks(sq, occ) | rook_attacks(sq, occ),
+        KING => king_attacks(sq),
+        _ => panic!("Bad piecetype for attack_bb()."),
+    }
+}
+
+fn bishop_attacks(sq: Square, occ: BitBoard) -> BitBoard {
+    BitBoard(ATTACKS[BISHOP_MAGICS[sq as usize].index(occ)])
+}
+
+fn rook_attacks(sq: Square, occ: BitBoard) -> BitBoard {
+    BitBoard(ATTACKS[ROOK_MAGICS[sq as usize].index(occ)])
+}
+
+fn knight_attacks(sq: Square) -> BitBoard {
+    KNIGHT_ATTACKS[sq as usize]
+}
+
+fn king_attacks(sq: Square) -> BitBoard {
+    KING_ATTACKS[sq as usize]
+}
+
+const fn init_non_slider_attacks(dirs: [Direction; 8]) -> [BitBoard; 64] {
+    let mut res = [BitBoard(0); 64];
+
+    let mut sq = 0;
+    while sq < SQUARE_NB {
+        let mut dir = 0;
+        while dir < 8 {
+            let to = (sq as i8 + dirs[dir]) as usize;
+            if to < SQUARE_NB && distance(sq as Square, to as Square) <= 2 {
+                res[sq] = res[sq].or(bb!(to));
+            }
+            dir += 1;
+        }
+        sq += 1;
+    }
+
+    res
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use scam::attacks::*;
 use scam::bitboard::*;
 use scam::types::*;
 use scam::*;
@@ -5,14 +6,13 @@ use scam::*;
 fn main() {
     let fen: &str = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
     let pos = position::Position::parse_fen(fen);
-    println!(
-        "{:b}",
-        Magic::bishop_attacks(F1, pos.color_bb[0] | pos.color_bb[1])
-    );
-    println!("{:b}", Magic::bishop_attacks(F1, BB_ZERO));
-    println!(
-        "{:b}",
-        Magic::rook_attacks(H1, pos.color_bb[0] | pos.color_bb[1])
-    );
-    println!("{:b}", Magic::rook_attacks(H1, BB_ZERO));
+
+    for pt in [KNIGHT, BISHOP, ROOK, QUEEN, KING].iter() {
+        println!("{:b}", attack_bb(*pt, H3, BitBoard(0)));
+    }
+
+    println!("{:b}", pawn_attack_bb(WHITE, H3));
+
+    let func = pawn_bb_attack_bb; // cool
+    println!("{:b}", func(WHITE, pos.piece_bb[PAWN as usize]));
 }
