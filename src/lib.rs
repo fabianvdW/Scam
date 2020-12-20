9f0def7674d9854ae39764fa9f5ee6895a5a7e7d
Terje <42723273+TerjeKir@users.noreply.github.com>
2020-12-20 06:08:11 +0100

    organize

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,8 @@
-use crate::position::Position;
-
 #[macro_use]
 pub mod bitboard;
 pub mod attacks;
 pub mod magics;
 pub mod r#move;
+pub mod perft;
 pub mod position;
 pub mod types;
-
-pub fn perft(pos: Position, depth: usize) -> u64 {
-    if depth == 0 {
-        return 1;
-    }
-    let mut res = 0;
-    let mut mv_list = r#move::MoveList::default();
-    pos.gen_pseudo_legals(&mut mv_list);
-    for mv in mv_list {
-        let mut new_pos = pos.clone();
-        if new_pos.make_move(mv) {
-            res += perft(new_pos, depth - 1);
-        }
-    }
-    res
-}
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,27 @@
-use scam::position::*;
+use scam::*;
 use std::io;
-use std::time::Instant;
-// use scam::*;
+
+fn uci() {
+    println!("id name Scam 0.0");
+    println!("id author Fabian von der Warth, Terje Kirstihagen");
+    println!("uciok")
+}
 
 fn main() {
     let stdin = io::stdin();
-    let mut line = String::new();
     loop {
-        line.clear();
+        let mut line = String::new();
         stdin.read_line(&mut line).unwrap();
-        let arg: Vec<&str> = line.split_whitespace().collect();
-        if line.is_empty() || arg.is_empty() {
+        let args: Vec<&str> = line.split_whitespace().collect();
+        if line.is_empty() || args.is_empty() {
             continue;
         }
-        let cmd = arg[0];
-        match cmd.trim() {
-            "uci" => println!("uciok"),
+        let cmd = args[0].trim();
+        match cmd {
+            "uci" => uci(),
             "isready" => println!("readyok"),
-            "perft" => {
-                let depth = arg[1].parse::<usize>().unwrap();
-                let fen = arg[2..].join(" ").to_string();
-                let pos = Position::parse_fen(&fen);
-                let now = Instant::now();
-                let count = scam::perft(pos, depth);
-                let after = Instant::now();
-                println!("{}", count);
-                let dur = after.duration_since(now);
-                let secs = dur.as_millis() as f64 / 1000.0;
-                println!(
-                    "{}",
-                    &format!("Time {} ({} nps)", secs, count as f64 / secs)
-                );
-            }
-            "quit" => {
-                break;
-            }
+            "perft" => perft::perft(args),
+            "quit" => break,
             _ => {}
         }
     }
