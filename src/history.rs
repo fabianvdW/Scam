1d5cf3cdde8ebd2eb9d0d169f132979ac6050251
fabianvdW <fabianvonderwarth@gmail.com>
2021-01-19 18:20:49 +0100

    Switch to a (faster?) array implementation
    BENCH=3904136

--- src/history.rs
+++ src/history.rs
@@ -1,27 +1,35 @@
 use crate::position::Position;
 
-#[derive(Default, Clone)]
+#[derive(Clone)]
 pub struct HashHist {
-    hist: Vec<u64>,
+    hist: [u64; 256],
+    pointer: usize,
+}
+impl Default for HashHist {
+    fn default() -> Self {
+        HashHist {
+            hist: [0; 256],
+            pointer: 0,
+        }
+    }
 }
 impl HashHist {
     pub fn clear(&mut self) {
-        self.hist.clear();
+        self.pointer = 0;
     }
 
     pub fn push(&mut self, pos: &Position) {
-        self.hist.push(pos.hash);
+        self.hist[self.pointer] = pos.hash;
+        self.pointer += 1;
     }
 
     pub fn pop(&mut self) {
-        self.hist.pop();
+        self.pointer -= 1;
     }
 
     pub fn is_2fold(&self, pos: &Position) -> bool {
         for i in 1..=pos.mr50 {
-            if self.hist.len() > i as usize
-                && self.hist[self.hist.len() - 1 - i as usize] == pos.hash
-            {
+            if self.pointer > i as usize && self.hist[self.pointer - 1 - i as usize] == pos.hash {
                 return true;
             }
         }
